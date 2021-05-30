f21a424fd78052f7428efe8885d1336c4a6012ac
Youssef-Hammad <youssef88999@gmail.com>
2021-05-30 12:03:22 +0200

    - Added register and login htmls (Still not designed - Included users.urls in the marker/urls.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def loginPage(request):
+    context = {}
+    return render(request,'users/login.html',context)
+
+def registerPage(request):
+    context = {}
+    return render(request,'users/register.html',context)
