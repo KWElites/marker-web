632158565de27a27aa65cf2b73e3b1e0ced144ca
Youssef-Hammad <youssef88999@gmail.com>
2021-05-31 12:53:10 +0200

    - Added logout functionality - Added authentication (user can't access home page unless they're logged in, can't access login/register if they're already logged in)

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('',views.homePage,name="home"),
     path('login/',views.loginPage,name="login"),
+    path('logout/',views.logoutUser,name="logout"),
     path('register/',views.registerPage,name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .forms import CreateUserForm
 
 # Create your views here.
 def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -22,8 +26,9 @@ def loginPage(request):
     return render(request,'users/login.html',context)
 
 def registerPage(request):
+    if request.user.is_authenticed:
+        return redirect('home')
     form = CreateUserForm()
-
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
@@ -37,7 +42,11 @@ def registerPage(request):
     context = {'form':form}
     return render(request,'users/register.html',context)
 
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
+@login_required(login_url='login')
 def homePage(request):
     context={}
     return render(request,'users/home.html',context)
