2d5df5d4d25643c5ffbe420101f763a3f4ac971f
Fady <fadymagued@hotmail.com>
2021-07-14 20:26:52 +0200

    Added Name to UserProfile | Added Name field in registeration

--- users/models.py
+++ users/models.py
@@ -5,3 +5,4 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(default = 'profilePics/defaultPP.jpg', upload_to = 'profilePics')
+    name = models.CharField(max_length=20, unique=True, default=None)
