fcf66ca651f57f546a11aea253694a18dc742655
Youssef-Hammad <youssef88999@gmail.com>
2021-07-18 01:17:18 +0200

    Made it so that unregistered users can view package details

--- users/views.py
+++ users/views.py
@@ -160,12 +160,12 @@ def uploadPage(request):
     }
     return render(request, 'users/upload.html', context)
 
-@login_required(login_url='login')
+#@login_required(login_url='login')
 def packageViewPage(request, pk):
     user = request.user
-    package = Package.objects.get(uId = user.id, id = pk)
+    package = Package.objects.get(id = pk)
     package_items = getPackageItems(package.packageItems, package.packageImages)
-    store = Store.objects.get(uId = user.id, id = package.sId.id)
+    store = Store.objects.get(id = package.sId.id)
     qr_code = generatePackageQRCode(request.get_host(), package.packageItems.url)
     
     context={
