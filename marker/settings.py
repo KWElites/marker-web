0c62fbde4716f9322383c786505ef3819bf45724
BouSenna <49456936+BouSenna@users.noreply.github.com>
2021-07-17 03:05:19 +0200

    updated static files dirs

--- marker/settings.py
+++ marker/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,8 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/users/static/',
+    os.path.join(BASE_DIR, 'statics'),
 ]
 
 MEDIA_URL = "/media/"
