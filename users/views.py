70fb6dada2df722771a1f95c97537c194346d77d
Youssef-Hammad <youssef88999@gmail.com>
2021-07-18 04:11:35 +0200

    Cleanup

--- users/views.py
+++ users/views.py
@@ -79,7 +79,6 @@ def homePage(request):
     return render(request,'users/home.html',context)
 
 def profilePage(request, username):
-    print(pretty_request(request))
     user = User.objects.get(username = username)
     userProfile = UserProfile.objects.get(user_id = user.id)
     user_packages = Package.objects.filter(uId = user.id) 
@@ -89,28 +88,6 @@ def profilePage(request, username):
         'user_packages': user_packages
     }
     return render(request, 'users/viewprofile.html', context)
-
-def pretty_request(request):
-    headers = ''
-    for header, value in request.META.items():
-        if not header.startswith('HTTP'):
-            continue
-        header = '-'.join([h.capitalize() for h in header[5:].lower().split('_')])
-        headers += '{}: {}\n'.format(header, value)
-
-    return (
-        '{method} HTTP/1.1\n'
-        'Content-Length: {content_length}\n'
-        'Content-Type: {content_type}\n'
-        '{headers}\n\n'
-        '{body}'
-    ).format(
-        method=request.method,
-        content_length=request.META['CONTENT_LENGTH'],
-        content_type=request.META['CONTENT_TYPE'],
-        headers=headers,
-        body=request.body,
-    )
 
 @login_required(login_url='login')
 def editProfile(request):
