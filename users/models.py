c10df8e4807eff77325c87fb9c3b28a5abb6f6bb
BouSenna <49456936+BouSenna@users.noreply.github.com>
2021-07-21 03:16:37 +0200

    updated package field labels for the upload form

--- users/models.py
+++ users/models.py
@@ -9,15 +9,15 @@ class UserProfile(models.Model):
 
 class Store(models.Model):
     uId = models.ForeignKey(User,on_delete=models.RESTRICT)
-    storeName = models.CharField(max_length=20)
+    storeName = models.CharField(max_length=20, verbose_name="Store Name")
 
 class Package(models.Model):
     uId = models.ForeignKey(User,on_delete=models.RESTRICT)
     sId = models.ForeignKey(Store,on_delete=models.RESTRICT, default=0)    
-    packageName = models.CharField(max_length=20)
-    packageDesc = models.CharField(max_length=512)
-    packageThumbnail = models.ImageField(default='package/thumbnails/defaultTN.png', upload_to='package/thumbnails')
-    packageItems = models.FileField(upload_to='package/packages', default='')
+    packageName = models.CharField(max_length=20, verbose_name="Package Name")
+    packageDesc = models.CharField(max_length=512, verbose_name="Package Description")
+    packageThumbnail = models.ImageField(default='package/thumbnails/defaultTN.png', upload_to='package/thumbnails', verbose_name="Package Thumbnail")
+    packageItems = models.FileField(upload_to='package/packages', default='', verbose_name="Package Zip File")
     packageImages = models.CharField(max_length=200, default='')
     # packageQRCode = models.CharField(max_length=200,default='')
 
